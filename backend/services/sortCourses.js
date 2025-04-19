fe3553fb95a51f406c12078feaf16c310413bd1a
Anwarsha k <anwarsha4790@gmail.com>
2025-04-19 14:26:56 +0530

    Finalizing merge with updated files and resolved conflicts

--- backend/services/sortCourses.js
+++
@@ -1,8 +0,0 @@
-const compareCourses = require('./compareCourses');
-
-function sortCourses(allCourses) {
-    // Filter, score, and sort the combined list
-    return compareCourses(allCourses);
-}
-
-module.exports = sortCourses;
